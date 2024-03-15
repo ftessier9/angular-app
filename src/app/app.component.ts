24c62ddb3b620269dae8c9140a436986670863e6
Felix Tessier <Felix.Tessier@solutionairgroup.com>
2024-03-15 15:00:23 -0500

    Add side nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { ProjectListComponent } from './project-list/project-list.component';
 import { ThemeToggleComponent } from './theme-toggle/theme-toggle.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent, ProjectListComponent, ThemeToggleComponent],
+  imports: [RouterOutlet, NavBarComponent, ProjectListComponent, ThemeToggleComponent, MatSidenavModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-app';
+  @ViewChild('sidenav') sidenav!: MatSidenav;
+
+  openSideNav(){
+    this.sidenav.open()
+  }
 }
